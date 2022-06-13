d9e325e99961e566b134324f0627734e8e8c1d26
Henrymelendez <henryomarm@gmail.com>
2022-06-12 22:53:46 -0400

    added some crud

--- RentalREST/src/main/resources/static/js/script.js
+++ RentalREST/src/main/resources/static/js/script.js
@@ -26,7 +26,8 @@ function loadAllProperties(){
 			}
 		}
 		else {
-			/// TODOO
+			diplaydisplayError("Error Creating property"+ xhr.status + " " + xhr.statusText);
+
 		}
 			
 	};
@@ -36,34 +37,160 @@ function loadAllProperties(){
 }
 
 
+function displayError(message){
+	let datDiv = document.getElementById('form-card');
+	datDiv.textContent = '';
+	datDiv.textContent = message;
+}
+
 
 
 
 function displayProperties(props){
-
+	
+	let table = document.getElementById('table1')
+	
+	
 	let tbody = document.getElementById('proprows');
-
 	
-	props.forEach(function(v,i,a){
+	
+	
+	for(let v of props){
 		let tr = document.createElement('tr');
-		let td = document.createElement('td');
-		let addressTD = document.createElement('td');
-		let rentalAmountTd = document.createElement('td');
-		let leaseTd = document.createElement('td');
+		tr.setAttribute('data-toggle', 'collapse');
+		tr.setAttribute('data-target', '#accordion'+v.id);
+		tr.className='';
 		
-		rentalAmountTd.textContent = v.rentalAmount;
-		td.textContent = v.id;
-		addressTD.textContent = v.propertyAddress;
-		leaseTd.textContent = v.leaseStatus;
-		tr.appendChild(td);
-		tr.appendChild(addressTD);
-		tr.appendChild(rentalAmountTd);
-		tr.appendChild(leaseTd);
+		tr.setAttribute('aria-control','accordion'+v.id)
+		
+		
+		let id = document.createElement('td');
+		id.textContent = v.id;
+		let address = document.createElement('td');
+		address.textContent = v.propertyAddress;
+		let renta = document.createElement('td');
+		renta.textContent = v.rentalAmount
+		let lease = document.createElement('td');
+		lease.textContent = v.leaseStatus;
+		
+		
+		tr.appendChild(id)
+		tr.appendChild(address)
+		tr.appendChild(renta)
+		tr.appendChild(lease)
 		tbody.appendChild(tr);
-	});
+		
+		let tr2 = document.createElement('tr');
+		let td2 = document.createElement('td');
+		td2.colSpan=4;
+		let div = document.createElement('div');
+		div.id="accordion"+v.id;
+		div.className="collapse show";
+		
+		
+		let p1 = document.createElement('li')
+		p1.textContent = v.note
+		
+		let p2 = document.createElement('li');
+		p2.textContent = v.purchaseAmount;
+		
+		let formd= document.createElement('form')
+		let hiddeninput = document.createElement('input');
+		hiddeninput.type="hidden";
+		hiddeninput.name='pId';
+		hiddeninput.value=v.id
+		let deletebutton = document.createElement('button')
+		deletebutton.className="btn btn-danger fa-solid fa-trash";
+		formd.appendChild(hiddeninput);
+		formd.appendChild(deletebutton)
+		
+		
+		 
+		div.appendChild(p1);
+		div.appendChild(p2);
+		div.appendChild(formd);
+		
+		td2.appendChild(div);
+		tr2.appendChild(td2);
+		
+		
+		
+		tbody.appendChild(tr2);
+		
+		
+	
+		}
+		
+		
+		
+	};
+  	
+
+
+
+
+
+
+function addProperty(evt){
+	
+	evt.preventDefault();
+	let form = document.myForm;
+	
+	let newProperty = {
+		propertyAddress: form.propertyAddress.value,
+		rentalAmount: form.rentalAmount.value,
+		purchaseDate: form.purchaseDate.value,
+		purchaseAmount: form.purchaseAmount.value,
+		note: form.note.value,
+		leaseStatus: form.leaseStatus.value
+	};
+	
+	
+	sendNewProp(newProperty);
+};
+
+		
+	
+function sendNewProp(sendNewProp){
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", 'api/types/1/properties')
+	
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState === 4){
+			if(xhr.status === 200 || xhr.status === 201){
+				let prop = JSON.parse(xhr.responseText);
+				displayProperties(prop);
+				
+			}
+			else {
+				displayError("Error Creating Property"+ xhr.status + " " + xhr.statusText);
+			}
+		}
+	}
+	xhr.setRequestHeader("Content-type", "application/json"); 
+	xhr.send(JSON.stringify(prop));
+	
+}
+
+
+function deleteProp(filmId){
+	let xhr = XMLHttpRequest();
+	xhr.open('DELETE',`api/types/1/properties/${filmId}`);
+	
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState === 4){
+			
+		}
+	};
+	
+	
+}
+
+
+	
+	
 	
 
-};
 
 
 const menuIconEl = $('.menu-icon');
